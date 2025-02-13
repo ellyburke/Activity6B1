0ae1711875a179abc351dbc2d7f6578b8d72ad38
ellyburke <117235750+ellyburke@users.noreply.github.com>
2025-02-13 14:48:21 -0500

    updating helloWorld function

--- activity.py
+++ activity.py
@@ -1,2 +1,8 @@
-def helloWord():
-    print("Hello World")
+def helloWord(name):
+    print("Hello " + name)
+
+def main():
+    name = input("Please enter your name: ")
+    helloWord(name)
+
+main()
