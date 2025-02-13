ebd233b6972277056f8ddff35433e14744d49473
ellyburke <117235750+ellyburke@users.noreply.github.com>
2025-02-13 15:30:53 -0500

    making changes

--- activity.py
+++ activity.py
@@ -1,8 +1,17 @@
-def helloWord(name):
+def helloWorld(name):
     print("Hello " + name)
+
+def addTax(price):
+    TAX = 1.13
+    return price*TAX
 
 def main():
     name = input("Please enter your name: ")
-    helloWord(name)
+    helloWorld(name)
+
+    # Adding tax to price
+    price = float(input("Enter item price: "))
+    taxedPrice = addTax(price)
+    print("The price with tax is: " + str(taxedPrice))
 
 main()
